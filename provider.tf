61d8c839990f55c0bb5996946c453beb279d0ba4
dhiru982 <117433657+dhiru982@users.noreply.github.com>
2022-12-17 11:56:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,8 +14,8 @@ provider "aws" {
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket = "dhirubucket"
+    bucket = "dhirubucket1"
     key    = "terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
